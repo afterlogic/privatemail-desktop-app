c0c9ed448812b7cb9c39e08cb586787f7127cbf1
Vasil Sokolov <vasil@afterlogic.com>
2019-08-09 12:03:45 +0300

    fixed quasar config issue

--- quasar.conf.js
+++ quasar.conf.js
@@ -173,7 +173,6 @@ module.exports = function (ctx) {
       extendWebpack (cfg) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
-        cfg.resolve.alias['@'] = resolve('src');
       },
 
       packager: {

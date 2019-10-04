c33e5fb3ce04d3d84762a5250bba4b5f2d150e8b
nadine-shi <nadine@afterlogic.com>
2019-10-04 11:25:10 +0300

    Message compose - HTML editor tools

--- src/pages/mail/MailCompose.vue
+++ src/pages/mail/MailCompose.vue
@@ -26,7 +26,7 @@
           </q-btn>
         </q-toolbar>
         <div class="col q-pa-md">
-          <q-list style="max-width: 500px;">
+          <q-list>
             <q-item>
               <q-item-section side center style="min-width: 100px;">
                 To
@@ -34,13 +34,25 @@
               <q-item-section>
                 <q-input outlined v-model="toText" :dense=true style="width: 100%;" />
               </q-item-section>
+              <q-item-section style="max-width: 100px;">
+                <a href="javascript:void(0)" v-show="!isCcShowed" @click="showCc">Show CC</a>
+                <a href="javascript:void(0)" v-show="!isBccShowed" @click="showBcc">Show BCC</a>
+              </q-item-section>
             </q-item>
-            <q-item>
+            <q-item v-show="isCcShowed">
               <q-item-section side center style="min-width: 100px;">
                 CC
               </q-item-section>
               <q-item-section>
                 <q-input outlined v-model="ccText" :dense=true style="width: 100%;" />
+              </q-item-section>
+            </q-item>
+            <q-item v-show="isBccShowed">
+              <q-item-section side center style="min-width: 100px;">
+                BCC
+              </q-item-section>
+              <q-item-section>
+                <q-input outlined v-model="bccText" :dense=true style="width: 100%;" />
               </q-item-section>
             </q-item>
             <q-item>
@@ -52,7 +64,54 @@
               </q-item-section>
             </q-item>
           </q-list>
-          <q-editor v-model="editortext" height="400px" class="col" />
+          <q-editor v-model="editortext" height="400px" class="col" :definitions="{
+              colors: {
+                tip: 'Font & background colors',
+                icon: 'text_format',
+                handler: changeColors
+              },
+              image: {
+                tip: 'Insert image',
+                icon: 'image',
+                handler: insertImage
+              },
+            }"
+            :toolbar="[
+              ['undo', 'redo'],
+              ['bold', 'italic', 'underline', 'strike'],
+              [{
+                list: 'no-icons',
+                options: [
+                  'default_font',
+                  'arial',
+                  'arial_black',
+                  'courier_new',
+                  'tahoma',
+                  'times_new_roman',
+                  'verdana'
+                ]
+              }, {
+                list: 'no-icons',
+                options: [
+                  'size-2',
+                  'size-3',
+                  'size-5',
+                  'size-7'
+                ],
+              },
+              'colors'],
+              ['unordered', 'ordered'],
+              ['link', 'image', 'removeFormat']
+            ]"
+            :fonts="{
+              arial: 'Arial',
+              arial_black: 'Arial Black',
+              courier_new: 'Courier New',
+              tahoma: 'Tahoma',
+              times_new_roman: 'Times New Roman',
+              verdana: 'Verdana'
+            }"
+          />
         </div>
       </div>
       <!-- <q-bar> -->
@@ -63,27 +122,43 @@
 <style></style>
 
 <script>
-// import FolderList from "components/FolderList.vue"
 
 export default {
-  name: "MailCompose",
+  name: 'MailCompose',
   components: {
-    // MessageViewer,
   },
   data () {
     return {
       dialog: false,
-      maximizedToggle: false,
+      maximizedToggle: true,
       editortext: '',
       toText: '',
       ccText: '',
-      subjectText: ''
+      bccText: '',
+      subjectText: '',
+
+      isCcShowed: false,
+      isBccShowed: false,
     }
   },
   methods: {
     openCompose () {
-      this.dialog = true;
-    }
-  }
-};
+      this.isCcShowed = false
+      this.isBccShowed = false
+      this.dialog = true
+    },
+    showCc () {
+      this.isCcShowed = true
+    },
+    showBcc () {
+      this.isBccShowed = true
+    },
+    changeColors () {
+      console.log('changeColors')
+    },
+    insertImage () {
+      console.log('insertImage')
+    },
+  },
+}
 </script>

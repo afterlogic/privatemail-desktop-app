cf8de001598fcfba009fd31d9a887b12f0d1cc61
Dmitrii Sikorskii <dmsikorskij@mail.ru>
2019-11-05 16:37:25 +0300

    Add highlighting for checked and selected contacts

--- src/pages/contacts/ContactsList.vue
+++ src/pages/contacts/ContactsList.vue
@@ -1,15 +1,19 @@
 <template>
   <q-list>
-    {{checkboxVal}}
-    <!-- :class="{'selected': checkboxVal.find(contact.UUID)}" -->
-    <div  v-for="contact in contacts.list" :key="contact.UUID" >
-      <q-item clickable v-ripple class="checked" @click="getContactByUUID(contact.UUID)" >
+    {{aCheckedList}}
+    <!-- :class="{'selected': aCheckedList.find(contact.UUID)}" -->
+    <div  v-for="contact in contacts.list" :key="contact.UUID">
+    <!-- {{isChecked(contact.UUID)}} -->
+      <q-item clickable v-ripple @click="getContactByUUID(contact.UUID)"
+          @focus="isSelected(contact.UUID)" :class="{checked: isChecked(contact.UUID), selected: contact.UUID === selected }">
         <q-item-section side class="">
-          <q-checkbox v-model="checkboxVal" :val="contact.UUID" />
+          <q-checkbox v-model="aCheckedList" :val="contact.UUID" />
         </q-item-section>
         <q-item-section>
-          <q-item-label lines="1">{{ contact.FullName !== '' ? contact.FullName : 'No name' }}</q-item-label>
-          <q-item-label lines="2">{{ contact.ViewEmail !== '' ? contact.ViewEmail : 'No email address' }}</q-item-label>
+          <q-item-label v-if="contact.FullName" lines="1">{{contact.FullName }}</q-item-label>
+          <q-item-label v-else lines="1" class="contact-notext">No name</q-item-label>
+          <q-item-label v-if="contact.ViewEmail" lines="2">{{contact.ViewEmail}}</q-item-label>
+          <q-item-label v-else lines="2" class="contact-notext">No email address</q-item-label>
         </q-item-section>
       </q-item>
       <q-separator /> 
@@ -17,17 +21,35 @@
   </q-list>
 </template>
 
-<style >
+<style lang="scss" scoped>
 /* .contact {
   /* padding: 8px 13px; */
 /* }  */
 
+
+
 hr.checked {
   background: #d6d6a9;
+}
+.checked {
+  background: var(--q-color-t-selection-alt);
+  
+  // .q-checkbox {
+  //   background: #fff;
+  // }
+}
+
+.selected {
+  background: var(--q-color-t-selection);
+  color: #fff;  
 }
 
 hr.selected {
   background: #6d5d7e;
+}
+
+.contact-notext {
+  color: #8c8989;
 }
 </style>
 
@@ -39,10 +61,13 @@ export default {
     return {
       page: 1,
       perPage: 20,
-      checkboxVal: [],
+      aCheckedList: [],
+      selected: null,
     }
   },
-
+  mounted: function() {
+     
+  },
   watch: {
     'currentStorage': function() {
       this.startAsyncGetContacts(true)
@@ -61,22 +86,23 @@ export default {
         this.contacts.list.forEach(element => {
           aContactsUUIDsList.push(element.UUID)
         }); 
-        this.checkboxVal = aContactsUUIDsList
+        this.aCheckedList = aContactsUUIDsList
       } else {
-        this.checkboxVal = []
+        this.aCheckedList = []
       }
     },
-    'checkboxVal': function() {
-      if (this.checkboxVal.length === this.contacts.list.length) {
+    'aCheckedList': function() {
+      if (this.aCheckedList.length === this.contacts.list.length) {
         this.$emit('allCheckChanged', true)
-      } else if (this.checkboxVal.length === 0) {
+      } else if (this.aCheckedList.length === 0) {
         this.$emit('allCheckChanged', false)
       }
+
     },
     'groupUUID': function() {
       if (this.groupUUID) {
         let contactsToMoveInGroup = []
-        this.checkboxVal.forEach(element => {
+        this.aCheckedList.forEach(element => {
           let contact = _.find(this.contacts.list, element.UUID)
           contact ? contactsToMoveInGroup.push(contact) : null
         })
@@ -114,6 +140,18 @@ export default {
     getContactByUUID(UUID) {
       this.$store.dispatch('contacts/getContactByUUID', UUID)
       console.log(UUID)
+    },
+    selectItem() {
+      this.selected = !this.selected
+    },
+    isChecked(UUID) {
+      // console.log('isChecked', _.find(this.aCheckedList, (el) => el === UUID));
+      return !!_.find(this.aCheckedList, (el) => el === UUID)
+    },
+    isSelected(UUID) {
+      console.log('isChecked', _.find(this.aCheckedList, (el) => el === UUID));
+      // return !!_.find(this.aCheckedList, (el) => el === UUID)
+      this.selected = UUID
     },
   },
 }

ae046a7980dd8926acd4e1184740ba099b22b935
nadine-shi <nadine@afterlogic.com>
2020-01-16 13:51:18 +0300

    Functionality for creating/removing shortcuts during application installation

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -11,6 +11,72 @@ import messagesDbManager from './mail/messages-db-manager.js'
 import contactsManager from './contacts/manager.js'
 import contactsDbManager from './contacts/db-manager.js'
 
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false
+  }
+
+  const childProcess = require('child_process')
+  const path = require('path')
+
+  const appFolder = path.resolve(process.execPath, '..')
+  const rootAtomFolder = path.resolve(appFolder, '..')
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'))
+  const exeName = path.basename(process.execPath)
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error
+
+    try {
+      spawnedProcess = childProcess.spawn(command, args, {
+        detached: true
+      })
+    } catch (error) {}
+
+    return spawnedProcess
+  }
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args)
+  }
+
+  const squirrelEvent = process.argv[1]
+  switch (squirrelEvent) {
+    case '--squirrel-updated':
+    case '--squirrel-install':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName])
+
+      setTimeout(app.quit, 1000)
+      return true
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName])
+
+      setTimeout(app.quit, 1000)
+      return true
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit()
+      return true
+  }
+}
+
+handleSquirrelEvent()
+
 /**
  * Set `__statics` path to static files in production;
  * The reason we are setting it here is that the path needs to be evaluated at runtime

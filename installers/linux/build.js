e2264acb36761504283384783eb5baa60405e88c
Vasil Sokolov <vasil@afterlogic.com>
2020-03-24 10:12:30 +0300

    added rpm packager

--- installers/linux/build.js
+++ installers/linux/build.js
@@ -1,10 +1,11 @@
-const createInstaller = require('electron-installer-debian')
+const createDebianInstaller = require('electron-installer-debian')
+const createRpmInstaller = require('electron-installer-redhat')
 
 const oPackageJsonData = require('../../package.json')
 const sVersion = oPackageJsonData.version
 const sBuild = oPackageJsonData.buildNumber
 
-const oOptions = {
+const oDebOptions = {
   src: '../../dist/electron/PrivateMail-linux-x64',
   dest: 'dist/',
   arch: 'amd64',
@@ -12,11 +13,28 @@ const oOptions = {
   productName: 'privatemail',
   icon: '../../src-electron/icons/linux-512x512.png',
 }
-console.log('options', oOptions)
 
-console.log('Creating package (this may take a while)')
-createInstaller(oOptions).then(function () {
-  console.log('The installer was succesfully created!')
+console.log('Debian package options', oDebOptions)
+console.log('Creating Debian package (this may take a while)')
+
+createDebianInstaller(oDebOptions).then(function () {
+  console.log('The installer for Debian was succesfully created!')
+}, function (oError) {
+  console.error(oError)
+})
+
+const oRpmOptions = {
+  src: '../../dist/electron/PrivateMail-linux-x64',
+  dest: 'dist/',
+  arch: 'x86_64',
+  name: 'privatemail-' + sVersion + '(' + sBuild + ')' + '-installer',
+  productName: 'privatemail',
+  icon: '../../src-electron/icons/linux-512x512.png',
+}
+
+console.log('Creating RedHat package (this may take a while)')
+createRpmInstaller(oRpmOptions).then(function () {
+  console.log('The installer for RedHat was succesfully created!')
 }, function (oError) {
   console.error(oError)
 })

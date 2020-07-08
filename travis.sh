f884986caf8fdf4345b20256199635f8150c6bc9
afterlogic <support@afterlogic.com>
2020-07-08 14:12:30 +0300

    fixed travis issue with file uploading via ftp

--- travis.sh
+++ travis.sh
@@ -24,27 +24,9 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo TASK: "$TASK"
 
-if [ "$TASK" = "build" ]; then
-	cd ${DIR}
-	
-	npm install -g gulp-cli
-	npm install
-
-	gulp styles --themes Default,DeepForest,Funny,Sand --build a
-	gulp js:build --build a
-	gulp js:min --build a
-	gulp test
-	
-	PRODUCT_VERSION=`cat VERSION`
-	
-	echo CREATE ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
-	
-	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/config.json data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt package.json composer.json composer.lock modules.json gulpfile.js pre-config.json privmail.png -x **/*.bak *.git*
-fi
-
 if [ "$TASK" = "test" ]; then
   newman run ./tests/tests.postman_collection.json -e ./tests/tests.postman_environment.json --env-var "Email=${TEST_EMAIL}" --env-var "Password=${TEST_PASSWORD}" --insecure --bail
-  echo Error: $?	
+  echo Error: $?
   if [ $? -eq 0 ]; then
     exit 1
   fi
@@ -54,18 +36,18 @@ if [ "$TASK" = "upload-deb-package" ]; then
 	cd ${DIR}
 	
 	FILENAME=`basename ./installers/linux/dist/*.deb`
-		
+	
 	echo UPLOAD INSTALLER FILE: "${FILENAME}"
 	
-	curl -v --ftp-create-dirs --retry 6 -T ./installers/linux/dist/${FILENAME} -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
+	curl -v --ftp-create-dirs --ftp-pasv --retry 6 -T ./installers/linux/dist/${FILENAME} -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
 fi
 
 if [ "$TASK" = "upload-rpm-package" ]; then
 	cd ${DIR}
 	
 	FILENAME=`basename ./installers/linux/dist/*.rpm`
-		
+	
 	echo UPLOAD INSTALLER FILE: "${FILENAME}"
 	
-	curl -v --ftp-create-dirs --retry 6 -T ./installers/linux/dist/${FILENAME} -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
+	curl -v --ftp-create-dirs --ftp-pasv --retry 6 -T ./installers/linux/dist/${FILENAME} -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
 fi

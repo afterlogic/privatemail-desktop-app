1259e145eb5ec949503e19a5a904abc3acffd9eb
Vadim <vtimofeev223@gmail.com>
2021-04-19 07:32:48 +0300

    fix bug actions import contacts "only vcf and ics files acceptable"

--- src/pages/contacts/ContactImport.vue
+++ src/pages/contacts/ContactImport.vue
@@ -24,6 +24,7 @@
                   flat
                   ref="uploader"
                   auto-upload
+                  accept=".csv, .vcf"
                   hide-upload-btn
                   :factory="importContacts"
                   @added="onFileAdded"
